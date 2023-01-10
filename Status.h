b1439336cec8b42e063df2e209ba232144798e4a
Ori <orinurieli1@gmail.com>
2023-01-10 13:57:25 +0200

    add Status

--- Status.h
+++ Status.h
@@ -1,14 +1,18 @@
 #ifndef __STATUS_H
 #define __STATUS_H
 #define _CRT_SECURE_NO_WARNINGS
+
 #include <ctime>
 #include <string>
 #include "Clock.h"
 
 class Status
 {
+private:
 	string _text;
 	Clock _time;
+
+	// todo: move Ctors to private - Abstract Class 
 public:
 	Status() : _text("") {}
 	Status(const string& text, Clock& time) : _text(text), _time(time) {}
@@ -16,12 +20,13 @@ public:
 	const string& getText() const { return _text; }
 	const Clock& getStatusTime() { return _time; }
 
-	void getStatusInfo(Status& newStatus);
+	virtual void getStatusInfo(Status& newStatus);
+	//virtual void display(); // todo
 
 	bool operator==(const Status& other) const;
 	bool operator!=(const Status& other) const;
 
-	~Status() {};
+	virtual ~Status() {};
 };
 
 #endif
